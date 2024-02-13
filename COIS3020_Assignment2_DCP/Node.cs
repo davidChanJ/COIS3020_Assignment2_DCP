a0b152d8055a25b44f8bf38f740a4230ad7e7842
davidChan <dchan@trentu.ca>
2024-02-13 13:14:47 -0500

    settings

---
+++ COIS3020_Assignment2_DCP/Node.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COIS3020_Assignment2_DCP
+{
+    public class Node
+    {
+        //Setting varibles
+        
+        public Node()
+        {
+            
+        }
+
+        public Node Build(string s, int i, int j)
+        {
+            return;
+        }
+        public Node Concatenate(Node p, Node q)
+        {
+            return;
+        }
+
+        public Node Split(Node p)
+        {
+            return;
+        }
+
+        public Node Rebalance()
+        {
+            return;
+        }
+    }
+}
---
+++ COIS3020_Assignment2_DCP/Rope.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COIS3020_Assignment2_DCP
+{
+    public class Rope
+    {
+        string S;
+        //Constructor
+        public Rope(string S)
+        {
+            this.S = S;
+        }
+
+        //Inserting and Deleting
+        public void Insert(string S, int i)
+        {
+
+        }
+        public void Delete(int i, int j)
+        {
+
+        }
+
+        //Substrings
+        public string subString(int i, int j)
+        {
+            return S[i, j];
+        }
+
+        public int Find(string S)
+        {
+            int i = 0;
+            if (S == null)
+                return i; //Return the first occurence of the character c    
+            return -1;
+        }
+
+        public char CharAt(int i)
+        {
+            return 'a';
+        }
+
+        public void Reverse()
+        {
+            //Possibly use for loop to find
+        }
+
+        public int Length()
+        {
+            return -1;
+        }
+
+        public string ToString()
+        {
+            return "";
+        }
+
+        public void PrintRope()
+        {
+            //Showing the root
+        }
+    }
+}
