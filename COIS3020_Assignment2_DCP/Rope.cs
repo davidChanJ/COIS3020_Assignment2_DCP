c6a8d2e421106ea80eda3111c3f26cc039c3e145
davidChan <dchan@trentu.ca>
2024-02-29 12:28:15 -0500

    Debugging for Rope.cs (Dejvid Version)

--- COIS3020_Assignment2_DCP/Rope.cs
+++ COIS3020_Assignment2_DCP/Rope.cs
@@ -1,21 +1,81 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.Design.Serialization;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using static COIS3020_Assignment2_DCP.Rope.Node;
 
-namespace COIS3020_Assignment2_DCP
-{
+namespace COIS3020_Assignment2_DCP {
+
+    public class RopeNode
+    {
+        public string Value { get; set; } // For leaf nodes, this holds the substring
+        public int Weight { get; set; } // Holds the total length of all characters in the left subtree
+        public RopeNode Left { get; set; }
+        public RopeNode Right { get; set; }
+
+        public RopeNode(string value)
+        {
+            Value = value;
+            Weight = value.Length;
+        }
+    }
     public class Rope
     {
         string S;
-        private Node root;
+        private RopeNode Root { get; set; }
         //Constructor
         public Rope(string S)
         {
             this.S = S;
-            root = Build(S, 0, S.Length-1);
+            Root = ConstructRope(S);
+        }
+
+        private RopeNode ConstructRope(string S)
+        {
+            // Base case for recursion: when S length is 10 or less
+            if (S.Length <= 10)
+            {
+                return new RopeNode(S);
+            }
+
+            // Split S into roughly two equal parts
+            List<RopeNode> nodes = new List<RopeNode>();
+            for (int i = 0; i < S.Length; i += 10)
+            {
+                int length = Math.Min(10, S.Length - i);
+                nodes.Add(new RopeNode(S.Substring(i, length)));
+            }
+
+            // Recursively combine nodes into a balanced tree
+            while (nodes.Count > 1)
+            {
+                List<RopeNode> combinedNodes = new List<RopeNode>();
+                for (int i = 0; i < nodes.Count; i += 2)
+                {
+                    if (i + 1 < nodes.Count)
+                    {
+                        RopeNode newNode = new RopeNode(null)
+                        {
+                            Left = nodes[i],
+                            Right = nodes[i + 1],
+                            Weight = nodes[i].Weight + (nodes[i].Left?.Weight ?? 0)
+                        };
+                        combinedNodes.Add(newNode);
+                    }
+                    else
+                    {
+                        combinedNodes.Add(nodes[i]); // Odd node out gets pushed to next level
+                    }
+                }
+                nodes = combinedNodes; // Prepare for next level
+            }
+
+            return nodes[0]; // The root of the constructed rope
         }
 
         //Inserting and Deleting
@@ -34,9 +94,9 @@ namespace COIS3020_Assignment2_DCP
         public string subString(int i, int j)
         {
             //Search in big string first
-
+            string result = "abc";
             //Searches until finding a smaller part
-            return S[i, j];
+            return result;
         }
 
         public int Find(string S)
@@ -79,21 +139,26 @@ namespace COIS3020_Assignment2_DCP
 
         public void PrintRope()
         {
-            PrintRope(root);
+            PrintRope(Root);
         }
 
-        private void PrintRope(Node node)
+        private void PrintRope(RopeNode node)
         {
             //Showing the root
-            if (Node is LeafNode leaf)
-                Console.WriteLine($"Lead Node: Length {leaf.Length}, String={leaf.Text}");
-            else if (Node is InternalNode internalNode)
+            //If it is a leaf
+            if(node.Left != null || node.Right != null )
+            {
+                Console.WriteLine($"Lead Node: Length {node.Left.Weight}, String={node.Left.ToString()}");
+            }
+               
+            //If it is a internal node
+            else
             {
                 Console.WriteLine("Internal Node: ");
                 Console.WriteLine("Left Child: ");
-                PrintRope(internalNode.Left);
+                //PrintRope(internalNode.Left);
                 Console.WriteLine("Right Child:");
-                PrintRope(internalNode.Right);
+                //PrintRope(internalNode.Right);
             }
         }
 
@@ -102,15 +167,49 @@ namespace COIS3020_Assignment2_DCP
         {
             public string s { get; set; }
             public int Length { get; set; }
-            public Node left, right;
-            public Node root { get; set; }
             public Node()
             {
                 this.s = s;
                 this.Length = 0;
-                this.left = left;
-                this.right = right;
-                this.root = root;
+
+            }
+
+            //Leaf and internal nodes
+            public class LeafNode : Node
+            {
+                public int Length { get; set; }
+                public string Text { get; set; }
+                public LeafNode(int length, string text)
+                {
+                    Length = length;
+                    Text = text;
+                }
+                public string GetString()
+                {
+                    return Text + Length.ToString();
+                }
+            }
+
+            public class InternalNode : Node
+            {
+                public Node Left { get; set; }
+                public Node Right { get; set; }
+                //Constructor
+                public InternalNode(int length)
+                {
+                    Left = null;
+                    Right = null;
+                }
+
+                public string GetString()
+                {
+                    return Left.GetString() + Right.GetString();
+                }
+            }
+
+            public string GetString()
+            {
+                return "";
             }
 
             //Making internal node & child nodes?
@@ -123,11 +222,14 @@ namespace COIS3020_Assignment2_DCP
                 return null;
 
             //Base case
+            int THERESHOLD = 5;
+            int j = 5;
+           
             if (e - i + 1 <= THERESHOLD)
-                return new LeafNode
+                return new LeafNode( (i-e), " " )
                 {
-                    Length = j - s + 1,
-                    TextReader = s.Substring(i, e - i + 1)
+                    Length = j - s.Length + 1,
+                    Text = s.Substring(i, e - i + 1)
                 };
             //Mid point
             int mid = i + (e - i) / 2;
@@ -136,10 +238,10 @@ namespace COIS3020_Assignment2_DCP
             Node rightNode = Build(s, mid + 1, e);
 
             //Return to internalNode:
-            return new InternalNode
+            return new InternalNode(S.Length)
             {
-                left = leftNode,
-                right = rightNode
+                Left = leftNode,
+                Right = rightNode
             };
         }
         public Node Concatenate(Node p, Node q)
@@ -148,17 +250,20 @@ namespace COIS3020_Assignment2_DCP
             if (p == null) return q;
             if (q == null) return p;
             //Check to reblance
-            if (Random.Next(p.Length + q.Length) < p.Length)
+            if ((p.Length + q.Length) < p.Length)
             {
-                left = Concatenate(p, (p.Length - 1));
-                right = q;
+                Node tempp = p; tempp.Length = p.Length - 1;
+                Node Left = Concatenate(p, tempp);
+                Node Right = q;
+
+                return new InternalNode(S.Length);
             }
             else
             {
-                return new InternalNode
+                return new InternalNode(S.Length)
                 {
-                    left = p,
-                    right = q
+                    Left = p,
+                    Right = q
                 };
             }
         }
@@ -170,6 +275,7 @@ namespace COIS3020_Assignment2_DCP
             //Creating 2 trees
 
             //Removing extra leaf nodes to balance
+            return null;
         }
 
         public Node Rebalance()
@@ -184,8 +290,25 @@ namespace COIS3020_Assignment2_DCP
             //subtract rope2 - rope1 - left rope
             //in
             //Concat left and right
-            Concatenate(left, right);
-            return;
+            Node rt = new Node(); //No idea what replacement
+            rt.Length = Root.Weight;
+            rt.s = Convert.ToString(Root.Value);
+            return rt = Rebalance(rt);
+        }
+
+        private Node Rebalance(Node node)
+        {
+            if(node is InternalNode internalNode)
+            {
+                Node left = Rebalance(internalNode.Left);
+                Node right = Rebalance(internalNode.Right);
+
+                return Concatenate(left, right);
+            }
+
+            //Returning
+            return node;
+            
         }
     }
 }
